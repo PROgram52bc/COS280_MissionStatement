b8b911051e16dbc326028a6e608f652199c78962
DavidDeng <daviddenghaotian@163.com>
2019-04-05 22:55:58 -0400

    added no repetition function to generate.py

--- syntax_based/generate.py
+++ syntax_based/generate.py
@@ -1,11 +1,16 @@
 import random
-def getRandom(componentClass):
+
+def getRandom(componentClass, noRepeat=True, usedValues=[]):
     leaf = componentClass.isLeaf()
     sequence = componentClass.collections if leaf else componentClass.constructs
     value = random.choice(sequence)
     if leaf:
+        while noRepeat and value in usedValues:
+            # NOTE: this will break if sequence contains too few words
+            value = random.choice(sequence)
+        usedValues.append(value)
         instance = componentClass(value)
     else:
-        value = [getRandom(cc) for cc in value]
+        value = [getRandom(cc, usedValues) for cc in value]
         instance = componentClass(value)
     return instance
