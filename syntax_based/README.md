1d961dbfe3552e7ca2bc6aaecf16b19b14f24ecf
David Deng <daviddenghaotian@163.com>
2019-04-06 02:46:28 -0400

    added readme, removed generate.py, fixed trivialSyntax

---
+++ syntax_based/README.md
@@ -0,0 +1,9 @@
+# How do I run the program?
+Execute
+```
+python3 missionStatementSyntax.py
+```
+
+# Program structure
+* `components.py` defines base classes for language components.
+* each `xxxSyntax.py` defines a set of syntax, on which texts are generated.
--- syntax_based/trivialSyntax.py
+++ syntax_based/trivialSyntax.py
@@ -1,25 +1,20 @@
 from components import LeafComponent, NodeComponent
-from generate import getRandom
 # Custom classes
 
 class Noun(LeafComponent):
     """ A Noun """
-    collections = LeafComponent.collections + ['John', 'Mary']
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    pass
+Noun.addToCollection("John", "Mary")
 
 class Verb(LeafComponent):
     """ A Verb """
-    collections = LeafComponent.collections + ['loves', 'hates']
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    pass
+Verb.addToCollection("loves", "hates")
 
 class Sentence(NodeComponent):
     """ A Sentence """
-    NodeComponent.addConstruct(Noun, Verb, Noun)
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+Sentence.addConstruct(Noun, Verb, Noun)
 
 if __name__ == "__main__":
-    obj = getRandom(Sentence)
+    obj = Sentence.random()
     print(obj.toHumanReadable())
